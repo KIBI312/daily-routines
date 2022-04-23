75c51cf2d0a5ca9f3f4f8ec1ff89fb0efbafbf0d
KIBI312 <isken00.kg@gmail.com>
2022-04-23 10:48:18 -0500

    init

---
+++ configserver/src/test/java/com/dailyservices/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailyservices.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
